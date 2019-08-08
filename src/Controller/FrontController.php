3a18ed744300da4d0c87ff8813364cd71f8ccc13
Wojtek <szypelt@seo-partner.pl>
2019-08-08 11:11:56 +0200

    Make template links working in Symfony and crated admincontroller

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -29,4 +29,36 @@ class FrontController extends AbstractController
     {
         return $this->render('front/video_details.html.twig');
     }
+
+    /**
+     * @Route("/search-results", methods={"POST"}, name="search_results")
+     */
+    public function videoResults()
+    {
+        return $this->render('front/search_results.html.twig');
+    }
+
+    /**
+     * @Route("/pricing", name="pricing")
+     */
+    public function pricing()
+    {
+        return $this->render('front/pricing.html.twig');
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register()
+    {
+        return $this->render('front/register.html.twig');
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login()
+    {
+        return $this->render('front/login.html.twig');
+    }
 }
