fef8d8814f4456e561f54d03eb973ffb3709f1e3
Wojtek <szypelt@seo-partner.pl>
2019-08-27 12:04:55 +0200

    sdasd

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -1,5 +1,6 @@
 <?php
 
+
 namespace App\Twig;
 
 use Twig\Extension\AbstractExtension;
@@ -28,7 +29,42 @@ class AppExtension extends AbstractExtension
     public function slugify($string)
     {
         $string = preg_replace("/ +/", "-", trim($string));
-        $string = mb_strtolower(preg_replace('/[^A-Za-z0-9-]+/', ' ', $string),'UTF-8');
+        $string = mb_strtolower(preg_replace('/[^A-Za-z0-9-]+/', '', $string), 'UTF-8');
         return $string;
     }
 }
+
+
+//
+//namespace App\Twig;
+//
+//use Twig\Extension\AbstractExtension;
+//use Twig\TwigFilter;
+//use Twig\TwigFunction;
+//
+//class AppExtension extends AbstractExtension
+//{
+//    public function getFilters(): array
+//    {
+//        return [
+//            // If your filter generates SAFE HTML, you should add a third
+//            // parameter: ['is_safe' => ['html']]
+//            // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
+//            new TwigFilter('slugify', [$this, 'slugify']),
+//        ];
+//    }
+//
+//    public function getFunctions(): array
+//    {
+//        return [
+//            new TwigFunction('function_name', [$this, 'doSomething']),
+//        ];
+//    }
+//
+//    public function slugify($string)
+//    {
+//        $string = preg_replace("/ +/", "-", trim($string));
+//        $string = mb_strtolower(preg_replace('/[^A-Za-z0-9-]+/', ' ', $string),'UTF-8');
+//        return $string;
+//    }
+//}
