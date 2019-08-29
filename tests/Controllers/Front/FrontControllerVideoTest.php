6dad3784e24602290c4e09d5b0fe9188cb291f37
Wojtek <szypelt@seo-partner.pl>
2019-08-29 10:14:10 +0200

    Push on server

---
+++ tests/Controllers/Front/FrontControllerVideoTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class FrontControllerVideoTest extends WebTestCase
+{
+    public function testSomething()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'Hello World');
+    }
+}
